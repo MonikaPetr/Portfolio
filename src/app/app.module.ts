b98c8a6a7e04cf3b7a823a1ebfa3e75e9f8972c9
MonikaPetr <petroniene.m@gmail.com>
2018-01-11 19:55:24 +0200

    V1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatMenuModule, MatToolbarModule, MatIconModule, MatProgressBarModule } from '@angular/material';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {RoundProgressModule} from 'angular-svg-round-progressbar';
+
+import { SkillsComponent } from './skills/skills.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    AboutComponent,
+    PortfolioComponent,
+    ContactComponent,
+    SkillsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    RoundProgressModule,
+
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/portfolio/portfolio.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NguCarousel } from '@ngu/carousel';
+
+@Component({
+  selector: 'app-portfolio',
+  templateUrl: './portfolio.component.html',
+  styleUrls: ['./portfolio.component.scss']
+})
+export class PortfolioComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+
+}
